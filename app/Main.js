2721f2d0b2e47e4b5234ef01db87051ca0231401
gwin12 <nwfrgodwin@gmail.com>
2024-03-07 10:22:54 +0100

    added routing

--- app/Main.js
+++ app/Main.js
@@ -1,21 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const { useState, useEffect } = React
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const { useState, useEffect } = React;
 
 
 // Components
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+
 
 
 function Main() {
     return (
-        <>
-            <Header />            
-            <HomeGuest />
+        <BrowserRouter>
+        
+            <Header />  
+            <Routes>
+                <Route path="/" element={<HomeGuest/>} />        
+                <Route path="/about-us" element={<About/>} />        
+                <Route path="/terms" element={<Terms/>} />        
+            </Routes>     
             <Footer />
-        </>
+
+        </BrowserRouter>
     )
 }
 
